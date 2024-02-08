7f589d22539f9499cdafddcd8dcfd0f633c4bd0c
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2024-02-08 23:17:30 +0100

    2/7: Podział Projektów i Ich Odpowiedzialności

--- GymManager.UI/Program.cs
+++ GymManager.UI/Program.cs
@@ -1,15 +1,21 @@
+using GymManager.Application;
+using GymManager.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseInfrastructure();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
