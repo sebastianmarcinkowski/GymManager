f814256d8a138819786bca011404c410a97bd50d
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2024-02-10 06:07:38 +0100

    2/9: Pierwsza Kwerenda

---
+++ GymManager.Application/Tickets/Queries/GetTicketById/GetTicketByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace GymManager.Application.Tickets.Queries.GetTicketById;
+
+public class GetTicketByIdQueryHandler : IRequestHandler<GetTicketByIdQuery, TicketDto>
+{
+    public async Task<TicketDto> Handle(
+        GetTicketByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var id = request.Id;
+
+        //todo: implement logic to get ticket by id
+
+        return new TicketDto
+        {
+            Id = id,
+            Name = "Ticket Name"
+        };
+    }
+}
---
+++ GymManager.Application/Tickets/Queries/GetTicketById/TicketDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GymManager.Application.Tickets.Queries.GetTicketById;
+
+public class TicketDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
